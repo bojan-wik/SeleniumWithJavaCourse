41a2476e832e3a2fb5356aadb36a90097a89edf1
bojan-wik <bojan.wik@protonmail.com>
2021-04-19 16:20:20 +0200

    Chapter10 -> ArrayListDemo

--- src/Chapter32/ArrayList.java
+++
@@ -1,9 +0,0 @@
-package Chapter32;
-
-public class ArrayList {
-
-    public static void main(String[] args) {
-
-        ArrayList arrayList = new ArrayList();
-    }
-}
---
+++ src/Chapter32/ArrayListDemo.java
@@ -0,0 +1,50 @@
+package Chapter32;
+
+import java.util.ArrayList;
+/**
+ * Przykład użycia ArrayList w Chapter8_9 -> AddProductsToCart_ImplicitExplicitWait.java
+ */
+public class ArrayListDemo {
+
+    public static void main(String[] args) {
+
+        /**
+         * Standard array - basic functionality in Java.
+         * fixed sized - wymaga określenia wielkości przy definiowaniu array i nie da się jej potem zmienić
+         */
+        //String[] array = {"Mietek", "Gieniu"};
+        String array[] = new String[2];
+        array[0] = "Mietek";
+        array[1] = "Gieniu";
+        /**
+         * array nie da się w prosty sposób wyprintować, zazwyczaj trzeba zastosować for-loop
+         */
+        System.out.println(array);
+        for (int i = 0; i < array.length; i += 1) {
+            System.out.println(array[i]);
+        }
+
+        /**
+         * ArrayList - part of Java collection framework.
+         * dynamic sized - określenie wielkości przy definiowaniu ArrayList jest opcjonalne i da się ją potem zmienić
+         */
+        ArrayList<String> arrayList = new ArrayList<String>(2);
+        arrayList.add("eloooo");
+        arrayList.add("rap");
+        /**
+         * ArrayList da się w prosty sposób wyprintować
+         */
+        System.out.println(arrayList);
+        /**
+         * Dodawanie i usuwanie
+         */
+        arrayList.add(1, "gangsta");
+        System.out.println(arrayList);
+        arrayList.remove(0);
+        System.out.println(arrayList);
+        arrayList.add("ziomy");
+        System.out.println(arrayList);
+        arrayList.remove("rap");
+        System.out.println(arrayList);
+    }
+}
