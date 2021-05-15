f368a849afc5707f85f7a07505756e8b16046044
bojan-wik <bojan.wik@protonmail.com>
2021-05-15 13:09:12 +0200

    CalendarHandleWithDefinedInput

--- src/Chapter11/CalendarHandleWithDefinedInput.java
+++ src/Chapter11/CalendarHandleWithDefinedInput.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class Calendar2 {
+public class CalendarHandleWithDefinedInput {
 
     public static void main(String[] args) {
 
@@ -20,16 +20,49 @@ public class Calendar2 {
         driver.get(url);
         driver.manage().window().maximize();
 
+        Integer searchedYear = 2022;
+        String searchedMonth = "Jun";
+        Integer searchedDay = 25;
+
         WebElement cookiePopupAcceptButton = driver.findElement(By.id("ez-accept-all"));
         cookiePopupAcceptButton.click();
 
         WebElement travelDatePicker = driver.findElement(By.id("travel_date"));
         travelDatePicker.click();
+        /**
+         * W DOM są łącznie 3 elementy z class='datepicker-switch', ale element do wyboru miesiąca występuje zawsze jako pierwszy, więc w tym przypadku
+         * identyfikuję właściwy element po jego indeksie
+         */
+        //WebElement monthPicker = driver.findElement(By.xpath("//th[@class='datepicker-switch'][1]"));
+        /**
+         * Rozwiązanie z kursu - xpath w relacji parent-child
+         */
+        WebElement monthPicker = driver.findElement(By.xpath("//div[@class='datepicker-days']//th[@class='datepicker-switch']"));
+        monthPicker.click();
+        WebElement yearPicker = driver.findElement(By.xpath("//div[@class='datepicker-months']//th[@class='datepicker-switch']"));
+        yearPicker.click();
 
-        Integer searchedDay = 13;
+        List<WebElement> availableYears = driver.findElements(By.xpath("//span[@class='year active' or @class='year']"));
+        for (int i = 0; i < availableYears.size(); i += 1) {
+            WebElement availableYear = availableYears.get(i);
+            if (availableYear.getText().equals(searchedYear.toString())) {
+                availableYear.click();
+                break;
+            }
+        }
+        
+        List<WebElement> availableMonths = driver.findElements(By.xpath("//span[@class='month active' or @class='month']"));
+        for (int i = 0; i < availableMonths.size(); i += 1) {
+            WebElement availableMonth = availableMonths.get(i);
+            if (availableMonth.getText().equals(searchedMonth)) {
+                availableMonth.click();
+                break;
+            }
+        }
+
         /**
          * W kursie użyte jest szukanie po classname=day, ale moim zdaniem jest to błędne rozwiązanie, ponieważ wtedy zwraca nam wszystkie dni, nawet te niedostępne (np. przeszłe),
-         * co w niektórych przypadkach generuje błędy.
+         * co w niektórych przypadkach generuje błędy np. w DOM są dwie daty 25, pierwsza jest niedostępna i to tą skrypt będzie próbował kliknąć.
          */
         //List<WebElement> availableDays = driver.findElements(By.className("day"));
         /**
@@ -40,7 +73,7 @@ public class Calendar2 {
         for (int i = 0; i < availableDays.size(); i += 1) {
             WebElement availableDay = availableDays.get(i);
             if (availableDay.getText().equals(searchedDay.toString())) {
-                System.out.println(availableDay.getText());
+                //System.out.println(availableDay.getText());
                 availableDay.click();
                 /**
                  * W tym przypadku trzeba zastosować break. Kalendarz na stronie działa w ten sposób, że po kliknięciu w daną datę, kalendarz się zamyka.
