975fbb2e8bfa770f25453972d0e40a3a08c5d6d0
bojan-wik <bojan.wik@protonmail.com>
2021-05-22 14:36:01 +0200

    TableGridHandle

--- src/Chapter12/TableGridHandle.java
+++ src/Chapter12/TableGridHandle.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -24,36 +25,32 @@ public class TableGridHandle {
          * Wyodrębniam jako WebElement pierwszą tabelę, potem w obrębie tej tabeli wyodrębniam jej 3cią kolumnę w której są wyniki, które chcę wylistować,
          * a potem zsumować i sprawdzić, czy wynik sumy na stronie jest poprawny.
          */
-        //WebElement table1 = driver.findElement(By.xpath("//*[@id='innings_1']/div[1]"));
         WebElement table1 = driver.findElement(By.cssSelector("div[class='cb-col cb-col-100 cb-ltst-wgt-hdr']"));
         List<WebElement> table1column3 = table1.findElements(By.cssSelector("div[class='cb-col cb-col-100 cb-scrd-itms'] div:nth-child(3)"));
+
+        int table1TotalValue = Integer.parseInt(driver.findElement(By.xpath("//div[text()='Total']/following-sibling::div")).getText());
+        System.out.printf("Total result presented in the page: %d", table1TotalValue).println();
+
         /**
          * Iteruję po elementach z 3ciej kolumny, wpisuję i < (table1column3.size()) - 2 bo nie chcę ostatnich dwóch elementów z tej kolumny,
-         * do ostatnich dwóch elementów muszę się dostać osobno.
+         * do ostatnich dwóch elementów muszę się dostać osobno. Parsuję potem każdy wynik do int i sumuję je wszystkie w zewnętrznej zmiennej
          */
         int table1ResultsCount = 0;
         for (int i = 0; i < (table1column3.size()) - 2; i += 1) {
-            String result = table1column3.get(i).getText();
-            System.out.println(result);
-            table1ResultsCount += Integer.parseInt(result);
+            int result = Integer.parseInt(table1column3.get(i).getText());
+            table1ResultsCount += result;
         }
         /**
          * Wartość z wiersza Extras wyciągam za pomocą xpath, gdzie szukam diva z tekstem 'Extras' i potem przechodzę do jego pierwszego sibilinga
          */
-        String table1ExtrasValue = driver.findElement(By.xpath("//div[text()='Extras']/following-sibling::div")).getText();
-        System.out.println(table1ExtrasValue);
-        table1ResultsCount += Integer.parseInt(table1ExtrasValue);
-        System.out.println(table1ResultsCount);
+        int table1ExtrasValue = Integer.parseInt(driver.findElement(By.xpath("//div[text()='Extras']/following-sibling::div")).getText());
+        table1ResultsCount += table1ExtrasValue;
 
-        String table1TotalValue = driver.findElement(By.xpath("//div[text()='Total']/following-sibling::div")).getText();
-        System.out.println(table1TotalValue);
-
-        //Assert
-
-        //List<WebElement> table1rows = table1.findElements(By.cssSelector("cb-col cb-col-100 cb-scrd-itms"));
-
-        //div[@id='innings_1']//div[contains(@class, 'cb-scrd-itms')]//div[contains(@class, 'text-bold')]
-        //div[contains(@class, 'cb-scrd-itms')]
+        /**
+         * Na końcu printuję sobie wynik, który sam policzyłem i sprawdzam assertem, czy są takie same.
+         */
+        System.out.printf("Total result counted: %d", table1ResultsCount).println();
+        Assert.assertEquals(table1ResultsCount, table1TotalValue);
 
         driver.quit();
     }
