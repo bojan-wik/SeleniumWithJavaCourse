adae1f13397e7feb20a1a3cc6a372614649779d7
bojan-wik <bojan.wik@protonmail.com>
2021-05-27 13:05:08 +0200

    AutosuggestiveDropdown3

--- src/Chapter12/AutosuggestiveDropdown3.java
+++ src/Chapter12/AutosuggestiveDropdown3.java
@@ -15,7 +15,7 @@ public class AutosuggestiveDropdown3 {
 
         System.setProperty("webdriver.chrome.driver", "C:\\Tools\\Webdrivers\\Chrome\\90\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         Actions action = new Actions(driver);
 
         String url = "https://www.makemytrip.com/";
@@ -25,34 +25,41 @@ public class AutosuggestiveDropdown3 {
         String searchedFromCity = "mumbai";
         String searchedToCity = "deline";
 
-        /*WebElement loginPopup = driver.findElement(By.xpath("//div[contains(@class, 'autopop')]"));
-        System.out.println(loginPopup.isDisplayed());*/
-
         /**
-         * Muszę znaleźć jakiś sposób na randomowo wyskakujący popup - raz wyskakuje, innym razem nie
-         * hint: https://intellipaat.com/community/6010/how-to-check-if-an-element-is-visible-with-webdriver
+         * Problem:
+         * podczas otwierania strony czasem wyskakuje LoginPopup, a czasem nie. Kiedy wyskakuje - muszę go najpierw odkliknąć.
+         * Kiedy go nie ma - mogę kontynuować wykonywanie skrypt. Próbowałem zrobić if-else z metodą isDisplayed(), albo sprawdzić, czy element
+         * jest != null, ale to nie działało, bo gdy popup nie wyskakiwał to wywalało mi exception, że takiego elementu nie można zlokalizować.
+         * Rozwiązanie:
+         * Gdy popup wyskakuje - wrzucam go do listy i sprawdzam, czy lista ma więcej elementów niż 0.
+         * Hint: https://intellipaat.com/community/27181/selenium-check-if-element-exists-test-if-element-is-present-using-selenium-webdriver
          */
-        if (driver.findElement(By.xpath("//div[contains(@class, 'autopop')]")) != null) {
-            System.out.println("present");
+        //Boolean isLoginPopupPresent = driver.findElement(By.xpath("//div[contains(@class, 'autopop')]")) != null;
+        //Boolean isLoginPopupPresent = driver.findElement(By.xpath("//div[contains(@class, 'autopop')]")).isDisplayed();
+        Boolean isLoginPopupPresent = driver.findElements(By.xpath("//div[contains(@class, 'autopop')]")).size() > 0;
+        if (isLoginPopupPresent) {
+            System.out.println("popup present");
+            driver.findElement(By.xpath("//li[@data-cy='account']")).click();
+
         } else {
-            System.out.println("no present");
+            System.out.println("popup not present");
         }
 
         WebElement fromCity = driver.findElement(By.id("fromCity"));
         fromCity.click();
         WebElement fromCitySearchbox = driver.findElement(By.xpath("//div[@role='combobox']//input[@type='text']"));
         /**
-         * sendKeys() method
+         * Z użyciem metodfd sendKeys()
          */
-        fromCitySearchbox.sendKeys(searchedFromCity);
+        /*fromCitySearchbox.sendKeys(searchedFromCity);
         fromCitySearchbox.sendKeys(Keys.ARROW_DOWN);
-        fromCitySearchbox.sendKeys(Keys.ENTER);
+        fromCitySearchbox.sendKeys(Keys.ENTER);*/
         /**
-         * Actions class
+         * Z użyciem klasy Actions
          */
-        /*action.moveToElement(fromCity).click().build().perform();
+        action.moveToElement(fromCity).click().build().perform();
         action.moveToElement(fromCitySearchbox).sendKeys(searchedFromCity).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).
-                build().perform();*/
+                build().perform();
 
         //driver.quit();
     }
