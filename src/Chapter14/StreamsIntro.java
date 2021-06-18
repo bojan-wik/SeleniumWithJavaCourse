59cb6d8c06520d66bf52deccbe66f6a639e47501
bojan-wik <bojan.wik@protonmail.com>
2021-06-18 13:00:08 +0200

    StreamsIntro

--- src/Chapter14/StreamsIntro.java
+++ src/Chapter14/StreamsIntro.java
@@ -23,7 +23,8 @@ public class StreamsIntro {
 
         int countNamesStartingWithA = 0;
         for (int i = 0; i < namesList.size(); i += 1) {
-            if (namesList.get(i).toLowerCase().startsWith("a")) {
+            String name = namesList.get(i);
+            if (name.startsWith("A")) {
                 System.out.println(namesList.get(i));
                 countNamesStartingWithA += 1;
             }
@@ -32,7 +33,7 @@ public class StreamsIntro {
     }
 
     // ad 1. Using Java Streams
-    //@Test
+    @Test
     public void streamFilter() {
 
         ArrayList<String> namesList = new ArrayList<String>();
@@ -42,6 +43,30 @@ public class StreamsIntro {
         namesList.add("Aleks");
         namesList.add("Arab");
 
-        // 10:20
+        /**
+         * Tworzenie i działanie Java Streams można podzielić na 3 etapy:
+         *
+         * 1) Create a stream
+         *  a) konwertuję wcześniej stworzoną listę do stream ('namesList.stream()...'),
+         *
+         * 2) Perform intermediate operations
+         *  a) na stworzonym streamie dokonuję operacji filtrowania z wykorzystaniem lambda expression ('...filter(name -> name.startsWith("A"))...')
+         *  b) lambda expression dzieli się na 2 części:
+         *   - lewą stronę: tu deklaruję parametr/zmienną ('name')
+         *   - prawą stronę: tu deklaruję body lambdy - warunek logiczny, który musi zostać spełniony, to co wcześniej było w if-else
+         *     ('name.startsWith("A")')
+         *
+         * 3) Perform terminal operation
+         * a) na dotychczasowym streamie wykonuję ostatnią akcję - zliczanie elementów, które spełniły wcześniej zadeklarowany warunek logiczny
+         *    ('...count()')
+         * b) terminal operation wykonuje się tylko, jeżeli intermediate operation zwróci true,
+         *    poza tym - bez terminal operation nie wykona się intermediate operation
+         *
+         * To wszystko deklaruję w zmiennej typu long (jeszcze nie wiem dlaczego, ale w int się nie da)
+         */
+
+        long countNamesStartingWithA = namesList.stream().filter(name -> name.startsWith("A")).count();
+        System.out.println(countNamesStartingWithA);
+        // 24:16
     }
 }
