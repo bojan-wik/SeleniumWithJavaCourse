1044172f6276a7b7f2d428fba7cf53f717e4e032
bojan-wik <bojan.wik@protonmail.com>
2021-06-22 15:45:07 +0200

    StreamsIntro

--- src/Chapter14/StreamsIntro.java
+++ src/Chapter14/StreamsIntro.java
@@ -3,13 +3,17 @@ package Chapter14;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 /**
- * 1) Count the number of names starting with letter 'A' in a list
+ * Zadanka:
+ * I. Count the number of names starting with letter 'A' in a list
+ * II. Print all the names which length is greater than 4
+ * III. Among the names which length is greater than 4 - print the first result
  */
 public class StreamsIntro {
 
-    // ad 1. Regular way (without using Java Streams)
+    // Regular way (without using Java Streams)
     @Test
     public void regular() {
 
@@ -21,18 +25,26 @@ public class StreamsIntro {
         namesList.add("Arab");
         //System.out.println(namesList);
 
+        // ad I.
         int countNamesStartingWithA = 0;
         for (int i = 0; i < namesList.size(); i += 1) {
             String name = namesList.get(i);
             if (name.startsWith("A")) {
-                System.out.println(namesList.get(i));
+                //System.out.println(namesList.get(i));
                 countNamesStartingWithA += 1;
             }
         }
         System.out.println(countNamesStartingWithA);
+
+        // ad II.
+        for (String name : namesList) {
+            if (name.length() > 4) {
+                System.out.println(name);
+            }
+        }
     }
 
-    // ad 1. Using Java Streams
+    // Using Java Streams
     @Test
     public void streamFilter() {
 
@@ -40,14 +52,17 @@ public class StreamsIntro {
         namesList.add("Wiktor");
         namesList.add("Adam");
         namesList.add("Robert");
-        namesList.add("Aleks");
+        namesList.add("Alex");
         namesList.add("Arab");
 
+        // ad I.
         /**
          * Tworzenie i działanie Java Streams można podzielić na 3 etapy:
          *
          * 1) Create a stream
          *  a) konwertuję wcześniej stworzoną listę do stream ('namesList.stream()...'),
+         *  b) istnieje też możliwość stworzenia od razu obiektu typu Stream np.
+         *     Stream.of("Wiktor", "Adam", "Robert", "Aleks", "Arab")
          *
          * 2) Perform intermediate operations
          *  a) na stworzonym streamie dokonuję operacji filtrowania z wykorzystaniem lambda expression ('...filter(name -> name.startsWith("A"))...')
@@ -64,9 +79,22 @@ public class StreamsIntro {
          *
          * To wszystko deklaruję w zmiennej typu long (jeszcze nie wiem dlaczego, ale w int się nie da)
          */
-
         long countNamesStartingWithA = namesList.stream().filter(name -> name.startsWith("A")).count();
-        System.out.println(countNamesStartingWithA);
-        // 24:16
+        //System.out.println(countNamesStartingWithA);
+
+        // ad II.
+        /**
+         * 1) 'namesList.stream()' - na tym etapie stream jest stworzony i ma 5 elementów (imion)
+         * 2) '.filter(name -> name.length() > 4)' - na tym etapie dokonana jest już intermediate operation i stream ma już tylko 2 elementy
+         * 3) '.forEach(name -> System.out.println(name))' - terminal operation wykonuje operacje na aktualnym streamie (z 2 imionami)
+         */
+        //namesList.stream().filter(name -> name.length() > 4).forEach(name -> System.out.println(name));
+
+        // ad III.
+        Optional<String> firstNameLongerThan4 = namesList.stream().filter(name -> name.length() > 4).findFirst();
+        //System.out.println(firstNameLongerThan4);
+        namesList.stream().filter(name -> name.length() > 4).limit(1).forEach(name -> System.out.println(name));
+
+        // 34:00
     }
 }
