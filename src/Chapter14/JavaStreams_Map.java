5388588f4c09766d0481a230811bbab8a92ad27d
bojan-wik <bojan.wik@protonmail.com>
2021-06-27 18:11:06 +0200

    JavaStreams_Map

--- src/Chapter14/JavaStreams_Map.java
+++ src/Chapter14/JavaStreams_Map.java
@@ -1,9 +1,11 @@
 package Chapter14;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -11,13 +13,16 @@ import java.util.stream.Stream;
  * I. Print names which end with 'a' with uppercase
  * II. Print names which start with 'a' with lowercase and sorted
  * III. Merge two arrayLists
+ * IV. Check whether certain values are in the list
+ * V. Store the results of stream operations in some list
+ * VI. Print unique and sorted values from the list
  */
 public class JavaStreams_Map {
 
     @Test
     public void streamMap() {
 
-        // ad 1.
+        // ad I.
         /**
          * W 'JavaStreams_Filter' tworzyłem najpierw ArrayList i ją potem dopiero konwertowałem do stream.
          * Istnieje możliwość stworzenia stream bezpośrednio.
@@ -31,17 +36,51 @@ public class JavaStreams_Map {
         /*Stream.of("Wiktoria", "Adam", "Robert", "Aleksa", "Arab").filter(name -> name.endsWith("a")).map(name -> name.toUpperCase()).
                 forEach(name -> System.out.println(name));*/
 
-        // ad 2.
+        // ad II.
         /**
          * Z wykorzystaniem metody sorted(), która stanowi terminal operation, jak wykorzystywany wcześniej np. count()
          */
         /*Stream.of("Wiktoria", "Robert", "Aleksa", "Arab", "Adam").filter(name -> name.startsWith("A")).map(name -> name.toLowerCase()).
                 sorted().forEach(name -> System.out.println(name));*/
 
-        // ad 3.
-
+        // ad III.
         List<String> drinks = Arrays.asList("Beer", "Water", "Wine", "Coke", "Juice");
         List<String> foods = Arrays.asList("Sandwich", "Pizza", "Kebab", "Apple", "Chocolate");
-        // 51:00
+        /**
+         * Z wykorzystaniem metody concat()
+         */
+        Stream<String> streamOfConcatenatedLists = Stream.concat(drinks.stream(), foods.stream());
+        //streamOfConcatenatedLists.sorted().forEach(list -> System.out.println(list));
+
+        // ad IV.
+        /**
+         * filter() zwraca rzeczy, których szukam, jako stringi, a mi zależy na otrzymaniu booleana true albo false
+         */
+        //streamOfConcatenatedLists.filter(i -> i.equals("Beer")).forEach(i -> System.out.println(i));
+        /**
+         * Do tego muszę wykorzystać metodę anyMatch()
+         */
+        boolean isBeerInTheList = streamOfConcatenatedLists.anyMatch(i -> i.equalsIgnoreCase("Beer"));
+        Assert.assertTrue(isBeerInTheList);
+    }
+
+    @Test
+    public void streamCollect() {
+
+        List<String> drinks = Arrays.asList("Beer", "Water", "Wine", "Coke", "Juice");
+        System.out.println(drinks);
+        //drinks.stream().filter(drink -> drink.startsWith("W")).forEach(drink -> System.out.println(drink));
+
+        // ad V.
+        /**
+         * Z wykorzystaniem metody collect(), która kolekconuje rezultaty wcześniejszych intermediate operations i je zapisuje
+         */
+        List<String> listOfDrinksStartingWithW = drinks.stream().filter(drink -> drink.startsWith("W")).collect(Collectors.toList());
+        System.out.println(listOfDrinksStartingWithW);
+
+        // ad VI.
+        List<Integer> listOfNumbers = Arrays.asList(3, 2, 2, 7, 5, 1, 9, 7);
+        List<Integer> listOfUniqueAndSortedNumbers = listOfNumbers.stream().distinct().sorted().collect(Collectors.toList());
+        System.out.println(listOfUniqueAndSortedNumbers);
     }
 }
