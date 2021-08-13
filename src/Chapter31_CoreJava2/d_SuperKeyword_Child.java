28e235a10874b1b40fc2fda46efa409cda7b5c1c
bojan-wik <bojan.wik@protonmail.com>
2021-08-13 12:56:10 +0200

    Chapter31_CoreJava2/d_SuperKeyword

---
+++ src/Chapter31_CoreJava2/d_SuperKeyword_Child.java
@@ -0,0 +1,50 @@
+package Chapter31_CoreJava2;
+
+/**
+ * Super Keyword - byt, który pozwala na odnoszenie się do obiektów z klasy parent. Powiązane z koncepcją dziedziczenia.
+ * Głównie używane z:
+ *      a) zmiennymi
+ *      b) metodami
+ *      c) konstruktorami
+ */
+public class d_SuperKeyword_Child extends d_SuperKeyword_Parent {
+
+    String text = "Text from child class";
+
+    public void demoMethod() {
+        System.out.println("Method from child class");
+    }
+
+    /**
+     * c) I tak samo jest z wywoływaniem konstruktorów
+     */
+    public d_SuperKeyword_Child() {
+        //super();
+        System.out.println("Constructor from child class");
+        //https://www.geeksforgeeks.org/super-keyword/
+    }
+
+    public void display() {
+        /**
+         * a) W tym przypadku zostanie wywołana zmienna text z klasy child. Działa tu zasada Overridingu, o której uczyłem się już wcześniej
+         * - jeżeli mam dokładnie takie same zmienne (ta sama nazwa, to samo data type) w klasie parent i w klasie child to w obrębie klasy child
+         * zmienna z klasy child nadpisuje tą z klasy parent.
+         */
+        System.out.println(text);
+        /**
+         * Co zrobić, aby wyprintować także text z klasy parent? Muszę skorzystać z keyworda 'super'.
+         */
+        System.out.println(super.text);
+        /**
+         * b) Tak samo jest z wywoływaniem funkcji.
+         */
+        demoMethod();
+        super.demoMethod();
+    }
+
+    public static void main(String[] args) {
+
+        d_SuperKeyword_Child d_superKeyword_child = new d_SuperKeyword_Child();
+        //d_superKeyword_child.display();
+    }
+}
---
+++ src/Chapter31_CoreJava2/d_SuperKeyword_Parent.java
@@ -0,0 +1,17 @@
+package Chapter31_CoreJava2;
+
+public class d_SuperKeyword_Parent {
+
+    // a)
+    String text = "Text from parent class";
+
+    // b)
+    public void demoMethod() {
+        System.out.println("Method from parent class");
+    }
+
+    // c)
+    public d_SuperKeyword_Parent() {
+        System.out.println("Constructor from parent class");
+    }
+}
