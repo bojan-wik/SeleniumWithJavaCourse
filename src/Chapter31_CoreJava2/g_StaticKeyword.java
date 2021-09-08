6d03d6cbecdc186c40c78e433b0484fd25b9160b
bojan-wik <bojan.wik@protonmail.com>
2021-09-08 15:47:58 +0200

    Chapter31_CoreJava2/g_StaticKeyword

--- src/Chapter31_CoreJava2/g_StaticKeyword.java
+++ src/Chapter31_CoreJava2/g_StaticKeyword.java
@@ -2,30 +2,39 @@ package Chapter31_CoreJava2;
 
 /**
  * 'Static' keyword to non-access modifier, który może być używany przy:
- *  - blokach,
- *  - zmiennych,
- *  - metodach,
- *  - zagnieżdżonych klasach.
+ *  a) zmiennych,
+ *  b) metodach,
+ *  c) blokach,
+ *  d) zagnieżdżonych klasach.
  */
 public class g_StaticKeyword {
 
     /**
-     * a) Class variable
+     * a) 'Static' keyword użyty przy zmiennej sprawia, że zmienna staje się class variable
      */
     static String race = "Human";
-
+    static String sex = "Man";
     /**
-     * b) Instance variable
+     * Tym samym zmienne utworzone na poziomie klasy, ale bez keyworda 'static' są instance variable
      */
     String name;
     String country;
 
     /**
-     * c) Local variable
+     * Tworzenie static variables jest możliwe TYLKO na poziomie klasy. Nie można ich tworzyć w obrębie np. metody, konstruktora
      */
     g_StaticKeyword(String name, String country){
         this.name = name;
         this.country = country;
+    }
+    /**
+     * b) Metoda z keywordem 'static'. W obrębie takiej static method mogę odwoływać się tylko do innych elementów typu static
+     * np. zmiennych, czy metod.
+     * Odwołanie się do zmiennej non-static da mi errora.
+     */
+    public static void displayVariablesStaticMethod() {
+        System.out.println(sex + "\n");
+        //System.out.println(name);
     }
 
     public void displayVariables() {
@@ -36,15 +45,17 @@ public class g_StaticKeyword {
 
     public static void main(String[] args) {
 
+        /**
+         * ad b) Wywołanie static method jest możliwe już przed stworzeniem obiektu klasy i bez odniesienia do takiego obiektu,
+         * ew. mogę odnieść się do samej klasy
+         */
+        displayVariablesStaticMethod();
+        //g_StaticKeyword.displayVariablesStaticMethod();
+        /**
+         * Jeżeli chcę wywołać non-static method muszę wcześniej stworzyć obiekt klasy i się do niego odnieść
+         */
         g_StaticKeyword classInstance1 = new g_StaticKeyword("Wiktor", "Poland");
         g_StaticKeyword classInstance2 = new g_StaticKeyword("Luc", "France");
-
-
-        /**
-         * Wywołuję wcześniej stworzoną metodę display(), która wyświetla mi wszystkie zmienne.
-         * Piersza zmienna 'race' jako class variable, jest współdzielona przez oba obiekty i jest od nich niezależna.
-         * Pozostałe dwie zmienne 'name' i 'country' zostały zdefiniowane na etapie tworzenia obiektów i są od nich zależne.
-         */
         classInstance1.displayVariables();
         classInstance2.displayVariables();
     }
