ed6ba6a71c87ebabde4b648214cdbf6f8ae55204
bojan-wik <bojan.wik@protonmail.com>
2021-09-16 10:19:19 +0200

    Chapter31_CoreJava2/i_Packages

---
+++ src/Chapter31_CoreJava2/i_Packages.java
@@ -0,0 +1,38 @@
+package Chapter31_CoreJava2;
+
+import Chapter30.FunctionOverriding;
+
+/**
+ * Package in Java – to sposób na enkapsulację zestawu klas, interfejsów, subpackages.
+ *      a) Keyword 'import' np. import package.className
+ *      b) Niektórych package nie trzeba specjalnie importować - są one ustawione jako defaultowe/globalne I uruchamiane razem z compilerem np. Java.lang
+ *      c) Inne trzeba specjalnie importować, żeby móc z nich korzystać np. Java.util
+ *
+ * Package types:
+ * 1) In-built
+ * 2) User-defined
+ */
+
+public class i_Packages {
+
+    public static void main(String[] args) {
+        /**
+         * Klasa 'A' może korzystać z klasy 'B' bezpośrednio przez stworzenie obiektu, tylko i wyłącznie wtedy, gdy obie klasy należą do tego samego package.
+         * Niech:
+         *      Class 'A' = bieżąca klasa 'i_Packages',
+         *      Class 'B' = 'e_ThisKeyword'
+         * Obie klasy należą do tego samego package = 'Chapter31_CoreJava2'
+         */
+        e_ThisKeyword classB = new e_ThisKeyword();
+        classB.sampleMethod();
+        /**
+         * Teraz w Klasie 'A' chcę skorzystać z klasy, która jest w innym package.
+         * Niech:
+         *      Class 'A' = bieżąca klasa 'i_Packages',
+         *      Class 'B1' = 'FunctionOverrriding' z package 'Chapter30'
+         * Poza stworzeniem obiektu klasy muszę jeszcze specjalnie zaimportować ten package i tą klasę ('import Chapter30.FunctionOverriding')
+         */
+        FunctionOverriding classB1 = new FunctionOverriding();
+        classB1.Roof();
+    }
+}
--- src/Chapter31_CoreJava2/i_Packages_Class1.java
+++
@@ -1,8 +0,0 @@
-package Chapter31_CoreJava2;
-
-public class i_Packages_Class1 {
-
-    public void class1Method() {
-        System.out.println("Method from Class 1");
-    }
-}
